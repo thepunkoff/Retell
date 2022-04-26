cd2ca6dd2449eef84ca4a79d21aa7616cf4ca2e5
zznty <94796179+zznty@users.noreply.github.com>
2022-04-26 22:09:32 +0700

    add logging to NullExceptionReportService

--- Vk2Tg/Services/NullExceptionReportService.cs
+++ Vk2Tg/Services/NullExceptionReportService.cs
@@ -1,10 +1,18 @@
-﻿using Vk2Tg.Abstractions.Services;
+﻿using Microsoft.Extensions.Logging;
+using Vk2Tg.Abstractions.Services;
 namespace Vk2Tg.Services;
 
 public class NullExceptionReportService : IExceptionReportService
 {
+    private readonly ILogger<NullExceptionReportService> _logger;
+    public NullExceptionReportService(ILogger<NullExceptionReportService> logger)
+    {
+        _logger = logger;
+    }
+    
     public Task SendExceptionAsync(Exception exception)
     {
+        _logger.LogDebug(exception, "Exception has been reported");
         return Task.CompletedTask;
     }
 }
