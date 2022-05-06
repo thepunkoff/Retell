1661827f107be612d017f8fa24da9aefe9827c9c
zznty <94796179+zznty@users.noreply.github.com>
2022-05-06 16:35:58 +0700

    add support for post filtering

--- Retell/RetellService.cs
+++ Retell/RetellService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Retell.Abstractions.Services;
 using Retell.Core;
+using Retell.Filtering;
 
 namespace Retell;
 
@@ -10,15 +11,18 @@ public class RetellService : BackgroundService
     private readonly ILogger<RetellService> _logger;
     private readonly IEnumerable<IPostSource> _sources;
     private readonly IEnumerable<IPostRenderer> _renderers;
+    private readonly IPostFilteringService _postFilteringService;
     private readonly IExceptionReportService _reportService;
     public RetellService(ILogger<RetellService> logger, 
         IEnumerable<IPostSource> sources, 
         IEnumerable<IPostRenderer> renderers,
+        IPostFilteringService postFilteringService,
         IExceptionReportService reportService)
     {
         _logger = logger;
         _sources = sources;
         _renderers = renderers;
+        _postFilteringService = postFilteringService;
         _reportService = reportService;
     }
 
@@ -31,6 +35,7 @@ public class RetellService : BackgroundService
             {
                 await foreach (var post in _sources.Select(source => source.GetPosts(token))
                                    .Merge()
+                                   .Where(post => _postFilteringService.Filter(post) is FilteringResult.ShouldShow)
                                    .WithCancellation(token))
                 {
                     await Task.WhenAll(_renderers.Select(renderer => renderer.RenderAsync(post, token)));
